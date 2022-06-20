d866305e2960cfe253e2ac68ea01fa0423d7e932
junjian.chen <junjian.chen@yitu-inc.intra>
2022-06-20 09:48:29 +0800

    push test

--- micro-service-cloud-provider-library-8002/src/main/java/net/biancheng/c/MicroServiceCloudProviderLibrary8002Application.java
+++ micro-service-cloud-provider-library-8002/src/main/java/net/biancheng/c/MicroServiceCloudProviderLibrary8002Application.java
@@ -10,5 +10,6 @@ public class MicroServiceCloudProviderLibrary8002Application {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroServiceCloudProviderLibrary8002Application.class, args);
+        System.out.println("Hello World");
     }
 }
